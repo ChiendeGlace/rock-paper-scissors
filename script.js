1b215ba9f0d9ce58c85bdbeab90a0caec3ab4fa3
ChiendeGlace <szymon.szostak72@gmail.com>
2022-08-27 11:05:35 +0200

    Add playRound function

---
+++ script.js
@@ -0,0 +1,25 @@
+const getComputerChoice = () => {
+  const options = ['Rock', 'Paper', 'Scissors'];
+  return options[Math.floor(Math.random() * options.length)]
+}
+
+const playRound = (playerSelection, computerSelection) => {
+  if(playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
+    return'You lose! Paper beats Rock';
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') {
+    return'You win! Rock beats Scissors';
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
+    return 'You win! Paper beats Rock';
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
+    return 'You lose! Scissors beat Paper';
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') {
+    return 'You win! Scissors beat Paper';
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
+    return 'You lose! Rock beats Scissors';
+  } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return 'Draw! You both choose the same answer';
+  }
+}
+
+let x = playRound('Scissors', getComputerChoice())
+console.log(x)
