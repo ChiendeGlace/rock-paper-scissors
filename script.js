ce1aa1d930057195654d923cf9cf60adb4b2d877
ChiendeGlace <szymon.szostak72@gmail.com>
2022-09-09 20:03:37 +0200

    Add UI interface

--- script.js
+++ script.js
@@ -2,31 +2,70 @@ const getComputerChoice = () => {
   const options = ['Rock', 'Paper', 'Scissors'];
   return options[Math.floor(Math.random() * options.length)]
 };
+const body = document.querySelector('body');
+const result = document.createElement('h2');
+result.style.textAlign = 'center';
+body.appendChild(result);
+const reset = document.createElement('button');
+reset.textContent = 'Play again';
+reset.style.display = 'block';
+reset.style.marginInline = 'auto';
 
-const playRound = (playerSelection, computerSelection) => {
-  if(playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
-    return'You lose! Paper beats Rock';
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') {
-    return'You win! Rock beats Scissors';
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
-    return 'You win! Paper beats Rock';
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
-    return 'You lose! Scissors beat Paper';
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') {
-    return 'You win! Scissors beat Paper';
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
-    return 'You lose! Rock beats Scissors';
-  } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    return 'Draw! You both chose the same answer';
-  } else {
-    return 'Choose the right option!';
-  }
-};
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+buttons.forEach(button => button.addEventListener('click', playGame));
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    let askUser = prompt('Choose between Rock, Paper and Scissors!');
-    let result = playRound(askUser, getComputerChoice());
-    console.log(result);
-  }
+  function playGame(e) {
+    e.target.classList.add('on');
+    let playerSelection = e.target.id;
+    const playRound = (playerSelection, computerSelection) => {
+      if(playerSelection === 'rock' && computerSelection === 'Paper') {
+        result.textContent = 'You lose! Paper beats Rock';
+        computerScore += 1;
+      } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        result.textContent = 'You win! Rock beats Scissors';
+        playerScore += 1;
+      } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        result.textContent = 'You win! Paper beats Rock';
+        playerScore += 1;
+      } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        result.textContent = 'You lose! Scissors beat Paper';
+        computerScore += 1;
+      } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        result.textContent = 'You win! Scissors beat Paper';
+        playerScore += 1;
+      } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        result.textContent = 'You lose! Rock beats Scissors';
+        computerScore += 1;
+      } else if (playerSelection === computerSelection.toLowerCase()) {
+        result.textContent = 'Draw! You both chose the same answer';
+      } 
+      if(playerScore === 5){
+        result.textContent = `You won 5 to ${computerScore}!`;
+        buttons.forEach(button => button.removeEventListener('click', playGame));
+        body.appendChild(reset);
+      } else if(computerScore === 5) {
+        result.textContent = `The computer won 5 to ${playerScore}!`;
+        buttons.forEach(button => button.removeEventListener('click', playGame));
+        body.appendChild(reset);
+      }
+    };
+    playRound(playerSelection, getComputerChoice());
+  };
+
+
+
+  reset.addEventListener('click', e => {
+    playerScore = 0;
+    computerScore = 0;
+    buttons.forEach(button => button.addEventListener('click', playGame));
+    result.textContent = '';
+    body.removeChild(reset);
+  });
+
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+function removeTransition(e) { 
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('on');
 };
